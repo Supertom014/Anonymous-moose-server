cdb7c406d69a305394b95d9872e342fab95f811e
unknown <thomas-wison@gmx.co.uk>
2015-06-14 13:42:12 +0100

    Added new time out threads that use a queue messaging system. These threads perform tasks when timing out, by placing a message in the queue.

--- timeout_thread.py
+++ timeout_thread.py
@@ -6,6 +6,8 @@
 #
 
 from threading import Thread
+import multiprocessing
+import queue
 
 
 class TimeoutThread(Thread):
@@ -25,6 +27,7 @@ class TimeoutThread(Thread):
         self.callback(self.callback_arg)
         return
 
+# Only used in dispatch.py
 class TimeoutThreadDisable(Thread):
     def __init__(self, lock, seconds, callback, callback_arg):
         super().__init__()
@@ -48,6 +51,37 @@ class TimeoutThreadDisable(Thread):
         self.lock.release_lock()
         return
 
+
+class TimeoutThreadDisableQueue(Thread):
+    def __init__(self, out_queue, seconds, callback_name, callback_arg):
+        super().__init__()
+        self.out_queue = out_queue
+        self.in_queue = multiprocessing.Queue()
+        self.seconds = seconds
+        self.callback_name = callback_name
+        self.callback_arg = callback_arg
+
+    def disable(self):
+        self.in_queue.put("disable")
+
+    def run(self):
+        import time
+        while True:
+            time.sleep(1)
+            self.seconds -= 1
+            if self.seconds == 0:
+                break
+        incoming = ""
+        try:
+            incoming = self.in_queue.get_nowait()
+        except(queue.Empty):
+            pass
+        if(incoming == "disable"):  # Quit before sending item to queue
+            return
+        self.out_queue.put({'name':self.callback_name, 'arg':self.callback_arg})
+        return
+
+# Only used in dispatch.py
 class MultiTimeoutThread(Thread):
     def __init__(self, lock, seconds, callbacks, callback_arg):
         super().__init__()
@@ -95,6 +129,58 @@ class MultiTimeoutThread(Thread):
                     self.called_callback[x] = True
                 else:
                     self.lock.release_lock()
+        return
+
+class MultiTimeoutThreadQueue(Thread):
+    def __init__(self, out_queue, seconds, callback_names, callback_arg):
+        super().__init__()
+        self.in_queue = multiprocessing.Queue()
+        self.out_queue = out_queue
+        self.seconds = list(seconds)
+        self.callback_names = list(callback_names)
+        self.callback_arg = callback_arg
+        self.called_callback = [False]*len(callback_names)
+        #for name in callback_names:
+        #    self.called_callback.update({name:{'time': seconds}})
+
+    def disable(self):
+        self.in_queue.put({"disable": True})
+
+    def set_times_reset(self, *seconds):
+        self.in_queue.put({"times": seconds})
+
+    def exit_check(self):
+        count = 0
+        for value in self.called_callback:
+            if value:
+                count += 1
+        if count == len(self.called_callback):
+            return True
+        return False
+
+    def run(self):
+        import time
+        while True:
+            time.sleep(1)
+            exit_thread = False
+            incoming = {}
+            try:
+                incoming = self.in_queue.get_nowait()
+            except queue.Empty:
+                pass
+            if "times" in incoming:
+                self.seconds = list(incoming["times"])
+                self.called_callback = [False] * len(incoming["times"])
+            if "disable" in incoming:
+                exit_thread = incoming["disable"]
+
+            if exit_thread or self.exit_check():
+                break
+            for x in range(0, len(self.seconds)):
+                self.seconds[x] -= 1
+                if self.seconds[x] <= 0 and not self.called_callback[x]:
+                    self.out_queue.put({'name':self.callback_names[x], 'arg':self.callback_arg})
+                    self.called_callback[x] = True
         return
 
 if __name__ == '__main__':
